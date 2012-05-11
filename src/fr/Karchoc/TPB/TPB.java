1c85d31f04ffc376390593b3a8b11b32a93ca967
Karchoc <karchoc@live.fr>
2012-05-11 23:25:39 +0200

    Added Writer

--- src/fr/Karchoc/TPB/TPB.java
+++ src/fr/Karchoc/TPB/TPB.java
@@ -1,5 +1,6 @@
 package fr.Karchoc.TPB;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +15,12 @@ public class TPB extends JavaPlugin{
 		log.info("Yo!");
 		log.info("Le plugin est chargé!");
 		getServer().getPluginManager().registerEvents(new TPBVacheTuee(), this);
+		try {
+			Writer.WriteFile("Test", "plugins\\MobGuard", "TPB.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
